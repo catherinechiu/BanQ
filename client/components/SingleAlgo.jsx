ea54ca0aa476a00a36c9374dfe05ccca666bcf5e
Catherine <cc2368@cornell.edu>
2020-07-23 18:58:04 -0700

    more styling. cursor pointer fixed. ok to present

--- client/components/SingleAlgo.jsx
+++ client/components/SingleAlgo.jsx
@@ -19,7 +19,7 @@ class SingleAlgo extends Component {
     return (
       <div className="single-algo">
         <h4>REDUCE</h4>
-        <div className="" >
+        <div className="info" >
           <p>Submitted by: WILL SENTANCE</p>
           <p>Difficulty: WARM-UP</p>
           <p>Prompt: Create a function, reduce, that takes an array and reduces the elements to a single value. The reduce function loops through the array and applies any operation that you can put into a function to each element in the array while keeping track of the outcome of each loop. In this way, we could use reduce to do things like sum all the numbers in an array or multiply them all together.
@@ -27,10 +27,14 @@ class SingleAlgo extends Component {
         </div>
         <br></br>
         <br></br>
+        <br></br>
+        <br></br>
 
         <div className="player" >
           <ReactPlayer url='https://www.youtube.com/watch?v=ELX-FBXZ-3g' style={{}} />
         </div>
+        <br></br>
+        <br></br>
         <br></br>
         <br></br>
         <div>
